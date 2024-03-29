3344954a211ce8489a205e0f22b3613ffbc143f7
현재민 / Jaemin Hyun <94438552+JAMJAMI98@users.noreply.github.com>
2024-03-29 11:12:41 +0900

    [Bronze III] Title: 윷놀이, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2490. 윷놀이/윷놀이.py
@@ -0,0 +1,13 @@
+for i in range(3) :
+  game = list(map(int, input().split()))
+
+  if game.count(0) == 1 :
+    print("A")
+  elif game.count(0) == 2 :
+    print("B")
+  elif game.count(0) == 3 :
+    print("C")
+  elif game.count(0) == 4 :
+    print("D")
+  else :
+    print("E")
