beff5d4b9e9ec5c47d6f66f529cad5c62c686cd1
현재민 / Jaemin Hyun <94438552+JAMJAMI98@users.noreply.github.com>
2024-07-12 13:52:27 +0900

    [Silver V] Title: 국회의원 선거, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1417. 국회의원 선거/국회의원 선거.py
+++ 백준/Silver/1417. 국회의원 선거/국회의원 선거.py
@@ -1,28 +1,23 @@
-import heapq
+n = int(input())
+vote = [int(input()) for _ in range(n)]
 
-# 입력 처리
-N = int(input().strip())
-votes = [int(input().strip()) for _ in range(N)]
+# 다솜이의 처음 득표 수
+dasom = vote[0]
 
-# 다솜이의 초기 득표 수
-dasom_votes = votes[0]
-# 다른 후보들의 득표 수 리스트 (최대 힙으로 사용)
-other_votes = [-v for v in votes[1:]]  # 음수로 저장하여 최대 힙처럼 사용
+# 다른 후보자의 득표 수
+other_vote = vote[1:]
 
-# 힙 구성
-heapq.heapify(other_votes)
+# 매수 인원
+people = 0
 
-# 매수한 사람의 수
-bribes = 0
+# 가장 높은 득표 수 보기위한 내림차순 정렬
+other_vote.sort(reverse=True)
 
-# 최대 힙에서 가장 큰 값을 가져와 다솜이의 득표 수와 비교
-while other_votes and dasom_votes <= -other_votes[0]:
-    # 가장 높은 득표수를 가진 후보에게서 1표 매수
-    max_votes = -heapq.heappop(other_votes)  # 가장 높은 득표수
-    max_votes -= 1  # 1표 매수
-    dasom_votes += 1
-    bribes += 1
-    # 다시 힙에 삽입
-    heapq.heappush(other_votes, -max_votes)
+while other_vote and dasom <= other_vote[0]:
+  other_vote[0] -= 1
+  dasom += 1
+  people += 1
 
-print(bribes)
+  other_vote.sort(reverse=True)
+
+print(people)
